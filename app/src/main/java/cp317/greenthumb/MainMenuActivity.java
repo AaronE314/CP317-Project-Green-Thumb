495551f14f37f6985fcb05029b1cb9675913ee24
Brendan <whebre@gmail.com>
2018-11-13 16:18:26 -0500

    Testing to ensure commits work.

--- app/src/main/java/cp317/greenthumb/MainMenuActivity.java
+++ app/src/main/java/cp317/greenthumb/MainMenuActivity.java
@@ -12,6 +12,7 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        //Test
     }
 
 }
