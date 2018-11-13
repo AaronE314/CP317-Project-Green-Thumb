15a8df40c95a7e85e8c2879e7c986f8ddd37a7e9
HallowedHalcyon <43186991+HallowedHalcyon@users.noreply.github.com>
2018-11-13 13:04:56 -0500

    Add files via upload

---
+++ User.js
@@ -0,0 +1,50 @@
+
+let User = class User {
+    constructor(userId) {
+        this.admin = false;
+        this.bans = [];
+        this.id = userId;
+    }
+    
+    // getters
+    getBans() {
+        return this.bans;
+    }
+    getId() {
+        return this.id;
+    }
+    isAdmin() {
+        return this.admin;
+    }
+    
+    ban(adminId) {
+        if (!this.admin) {
+            let expiration = new Date();
+            let numBans = bans.length;
+            expiration.setDate(expiration.getDate() + (1 + numBans) * 7);
+            let ban = class{
+                constructor(adminId, expiration) {
+                    this.adminId = adminId;
+                    this.expiration = expiration;
+                }
+            }
+            this.bans[numBans] = ban;
+        }
+    }
+    isBanned() {
+        let len = this.bans.length;
+        if (len > 0) {
+            let ban = this.bans[len - 1];
+            let today = new Date();
+            let bannedToday = dates.compare(ban.expiration, today); // do bans contain adminId still
+            if (bannedToday == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    makeAdmin() {
+        this.admin = true;
+    }
+    
+}
