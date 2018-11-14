8744d048dbc968b662308014e02da5a9c6130877
Aaron Exley <aaronde314@gmail.com>
2018-11-14 15:52:56 -0500

    added base structure of ml classes

---
+++ backend/machineLearning/TFIdentifier.js
@@ -0,0 +1,73 @@
+
+/**@abstract
+ * 
+ * @author Aaron Exley
+ * @author Justin Harrott
+ * @author Jhon Duphny
+ * 
+ * @description This class is designed to Identify a given image,
+ * and return the results as an array of detections
+ * 
+ */
+class TFIdentifier {
+
+    /**
+     * @author Jhon Duphny
+     * 
+     * Add description ...
+     */
+    static gatherImages() {
+
+    }
+
+    /**
+     * @author Aaron Exley
+     * 
+     * Add description ...
+     * 
+     * @param image
+     */
+    static predict(image) {
+
+    }
+
+    /**
+     * @author Justin Harrott
+     * 
+     * Add description ...
+     */
+    static retrain() {
+
+    }
+
+    /**
+     * @author Justin Harrott
+     * 
+     * Add description ...
+     * 
+     * @param time
+     */
+    static scheduleTraining(time) {
+
+    }
+
+    /**
+     * @author Jhon Duphny
+     * 
+     * Add description ...
+     */
+    static _loadNewModel() {
+
+    }
+
+    /**
+     * @author Aaron Exley
+     * 
+     * Add description ...
+     * @param image
+     */
+    static _preProcessImage(images) {
+
+    }
+
+}
---
+++ backend/machineLearning/TFTrainer.js
@@ -0,0 +1,41 @@
+
+/**@abstract
+ * 
+ * @author Ben Keunen
+ * @author Joseph Myc
+ * 
+ * @description This class is designed to Identify a given image,
+ * and return the results as an array of detections
+ * 
+ */
+class TFTrainer {
+
+    /**
+     * @author Joseph Myc
+     * 
+     * Add description ...
+     */
+    static convertFormat() {
+        
+    }
+
+    /**
+     * @author Joseph Myc
+     * 
+     * Add description ...
+     * @param model
+     */
+    static saveModel(model) {
+
+    }
+
+    /**
+     * @author Ben Keunen
+     * 
+     * Add description ...
+     * @param model
+     */
+    static trainModel(model) {
+
+    }
+}
