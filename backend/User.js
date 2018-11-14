768965a8394780dd3b9e3d5f21d9bab10bc985c4
HallowedHalcyon <43186991+HallowedHalcyon@users.noreply.github.com>
2018-11-13 23:52:28 -0500

    Update User.js
    Fixed User to work with new status of Ban
    Fixed attributes to be private
    Fixed isBanned method
    Note: Trying to run on VSCode with the export statement causes an error. Did all the testing with "let User = class User" which does run.

--- backend/User.js
+++ backend/User.js
@@ -1,60 +1,106 @@
+"use strict";
+import Ban from './Ban.js'
+
 /**
+ * @description The User class
  * @author Saje Bailey
  */
-export let User = class User {
+export default class User {
+
     /**
-     * @param {int} userId
+     * @param {Ban[]} bans The list of Bans associated with the User
+     * @param {int} userId The ID of the User
      */
-    constructor(userId) {
-        this.admin = false;
-        this.bans = [];
-        this.id = userId;
-    }
-    
-    // getters
-    getBans() {
-        return this.bans;
-    }
-    getId() {
-        return this.id;
-    }
-    isAdmin() {
-        return this.admin;
-    }
-    /**
-     * @param {int} adminId
-     */
-    ban(adminId) {
-        if (!this.admin) {
+    constructor(userId, bans) {
+        // PRIVATE constant and attributes.
+        let _banBase = 3;
+        let _bans = (bans == null ? [] : bans);
+        let _id = userId;
+        
+        // PUBLIC methods.
+        this.getBans = getBans;
+        this.getId = getId;
+        this.ban = ban;
+        this.isBanned = isBanned;
+
+        // PUBLIC method definitions.
+        // getters
+        /**
+         * @returns {Ban[]} _bans The list of Bans associated with the User.
+         */
+        function getBans() {
+            return _bans;
+        }
+        /**
+         * @returns {int} _id The ID of the User.
+         */
+        function getId() {
+            return _id;
+        }
+        
+        /**
+         * @param {int} adminId The ID of the Admin banning the User.
+         */
+        function ban(adminId) {
             let expiration = new Date();
-            let numBans = bans.length;
-            expiration.setDate(expiration.getDate() + Math.pow(7, (numBans)));
-            let ban = class{
-                constructor(adminId, expiration) {
-                    this.adminId = adminId;
-                    this.expiration = expiration;
+            let numBans = _bans.length;
+            expiration.setDate(expiration.getDate() + Math.pow(_banBase, (numBans)));
+            
+            // call function in the DB Interface with appropreate constructor arguments
+            // this call to Ban constructor is temporary for testing
+            let b = new Ban(adminId, expiration);
+
+            _bans[numBans] = b;
+        }
+    
+    /**
+     * @returns {boolean} banned true if the User is currently banned, false otherwise
+     */
+        function isBanned() {
+            let len = _bans.length;
+            let banned = false;
+            
+            // if the user has been banned
+            if (len > 0) {
+                let ban = _bans[len - 1];
+                let ex = ban.getDate()
+                let today = new Date();
+
+                // If the ban goes into another year
+                if (ex.getFullYear() > today.getFullYear()) {
+                    banned = true;
+                    // console.log("year");
+                }
+                
+                else if (ex.getFullYear() == today.getFullYear()) {
+                    // if the ban is in a future month
+                    if (ex.getMonth() > today.getMonth()) {
+                        banned = true;
+                        // console.log("month");
+                    }
+                    else if (ex.getMonth() == today.getMonth()) {
+                        // If today is not equal to or less than the ban
+                        if (ex.getDate() > today.getDate()) {
+                            banned = true;
+                            // console.log("day");
+                        }
+                    }
                 }
             }
-            this.bans[numBans] = ban;
-        }
+            return banned;
+        } 
     }
-    /**
-   * @returns {boolean} banned;
-   */
-    isBanned() {
-        let len = this.bans.length;
-        if (len > 0) {
-            let ban = this.bans[len - 1];
-            let today = new Date();
-            let bannedToday = dates.compare(ban.expiration, today); // do bans contain adminId still
-            if (bannedToday == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-    makeAdmin() {
-        this.admin = true;
-    }
+
     
 }
+
+// Mock Ban class for testng purposes
+// let Ban = class Ban {
+//     constructor(adminId, expiration) {
+//         this.adminId = adminId;
+//         this.expiration = expiration;
+//     }
+//     getDate() {
+//         return this.expiration;
+//     }
+// }
