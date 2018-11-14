2457eb10565d319683efd2c0225d794ac3e52c6a
Nathaniel Carr <carr0170@mylaurier.ca>
2018-11-14 11:04:32 -0500

    Shortened some User.js methods and moved constants outside constructor.

--- backend/User.js
+++ backend/User.js
@@ -1,22 +1,22 @@
 "use strict";
 import Ban from './Ban.js'
 
+const BAN_BASE = 3;
+
 /**
- * @description The User class
+ * @desc The User class
  * @author Saje Bailey
  */
 export default class User {
-
     /**
      * @param {Ban[]} bans The list of Bans associated with the User
      * @param {int} userId The ID of the User
      */
     constructor(userId, bans) {
-        // PRIVATE constant and attributes.
-        let _banBase = 3;
-        let _bans = (bans == null ? [] : bans);
+        // PRIVATE attributes.
+        let _bans = (bans === undefined ? [] : bans);
         let _id = userId;
-        
+
         // PUBLIC methods.
         this.getBans = getBans;
         this.getId = getId;
@@ -26,81 +26,33 @@ export default class User {
         // PUBLIC method definitions.
         // getters
         /**
-         * @returns {Ban[]} _bans The list of Bans associated with the User.
+         * @returns {Ban[]} The list of Bans associated with the User.
          */
         function getBans() {
             return _bans;
         }
         /**
-         * @returns {int} _id The ID of the User.
+         * @returns {int} The ID of the User.
          */
         function getId() {
             return _id;
         }
-        
         /**
          * @param {int} adminId The ID of the Admin banning the User.
          */
         function ban(adminId) {
             let expiration = new Date();
-            let numBans = _bans.length;
-            expiration.setDate(expiration.getDate() + Math.pow(_banBase, (numBans)));
-            
+            expiration.setDate(expiration.getDate() + Math.pow(BAN_BASE, (numBans)));
+
             // call function in the DB Interface with appropreate constructor arguments
             // this call to Ban constructor is temporary for testing
-            let b = new Ban(adminId, expiration);
-
-            _bans[numBans] = b;
+            _bans[_bans.length] = new Ban(adminId, expiration);
         }
-    
-    /**
-     * @returns {boolean} banned true if the User is currently banned, false otherwise
-     */
+        /**
+         * @returns {boolean} true iff the User is currently banned.
+         */
         function isBanned() {
-            let len = _bans.length;
-            let banned = false;
-            
-            // if the user has been banned
-            if (len > 0) {
-                let ban = _bans[len - 1];
-                let ex = ban.getDate()
-                let today = new Date();
-
-                // If the ban goes into another year
-                if (ex.getFullYear() > today.getFullYear()) {
-                    banned = true;
-                    // console.log("year");
-                }
-                
-                else if (ex.getFullYear() == today.getFullYear()) {
-                    // if the ban is in a future month
-                    if (ex.getMonth() > today.getMonth()) {
-                        banned = true;
-                        // console.log("month");
-                    }
-                    else if (ex.getMonth() == today.getMonth()) {
-                        // If today is not equal to or less than the ban
-                        if (ex.getDate() > today.getDate()) {
-                            banned = true;
-                            // console.log("day");
-                        }
-                    }
-                }
-            }
-            return banned;
-        } 
+            return (_bans.length > 0) && (new Date().getTime() < new Date(_bans[_bans.length - 1]).getTime());
+        }
     }
-
-    
 }
-
-// Mock Ban class for testng purposes
-// let Ban = class Ban {
-//     constructor(adminId, expiration) {
-//         this.adminId = adminId;
-//         this.expiration = expiration;
-//     }
-//     getDate() {
-//         return this.expiration;
-//     }
-// }
