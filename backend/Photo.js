4f2d0f61e0a3efd2faec960b6eede56492f8f544
Nathaniel Carr <carr0170@mylaurier.ca>
2018-11-13 20:00:07 -0500

    Fixed syntax errors in Photo.js.

--- backend/Photo.js
+++ backend/Photo.js
@@ -1,218 +1,211 @@
 /**
+ * @desc The Photo class.
  * @author Adam Cassidy
  */
 class Photo {
-  let this.downvoteIds = [];
-  let this.upvoteIds = [];
-  let this.id = -1;
-  let this.plantId = -1;
-  let this.userId = -1;
-  let this.uploadDate = Date;
-  let this.image = [];
+    /**
+     * @desc The Photo class constructor.
+     * @author Adam Cassidy
+     * @param {int} id The ID of the Photo.
+     * @param {int} plantId The ID of the corresponding Plant.
+     * @param {int} userId The ID of the User who uploaded this Photo.
+     * @param {byte[]} image The byte array for the image.
+     * @constructor
+     */
+    constructor(id, plantId, userId, image) {
+        // PRIVATE attributes.
+        this.downvoteIds = [];
+        this.upvoteIds = [];
+        this.id = id;
+        this.plantId = plantId;
+        this.userId = userId;
+        this.uploadDate = new Date();
+        this.image = image;
 
-  /**
-   * @param {int} plantId
-   * @param {int} userId
-   * @param {int} id
-   * @param {byte[]} image
-   * @constructor
-   */
-  constructor(plantId, userId, id, image){
-    this.downvoteIds = downvoteIds;
-    this.upvoteIds = upvoteIds;
-    this.id = id;
-    this.plantId = plantId;
-    this.userId = userId;
-    this.uploadDate = new Date();
-    this.image = image;
-  }
-  
-  /**
-   * @returns {int[]} downvoteIds;
-   */
-  function getDownvoteIds(){
-    return this.downvoteIds;
-  }
+        // PUBLIC methods.
+        this.getId = getId;
+        this.getPlantId = getPlantId;
+        this.getUserId = getUserId;
+        this.getUploadDate = getUploadDate;
+        this.getUpvoteIds = getUpvoteIds;
+        this.getDownvoteIds = getDownvoteIds;
+        this.userUpvoted = userUpvoted;
+        this.userDownvoted = userDownvoted;
+        this.getVoteSum = getVoteSum;
+        this.vote = vote;
 
-  /**
-   * @returns {int} Id;
-   */
-  function getId(){
-    return this.Id;
-  }
-
-  /**
-   * @returns {int} plantId;
-   */
-  function getPlant(){
-    return this.plantId;
-  }
-
-  /**
-   * @returns {Date} uploadDate;
-   */
-  function getUploadDate(){
-    return this.uploadDate;
-  }
-
-  /**
-   * @returns {int[]} upvoteIds;
-   */
-  function getUpvoteIds(){
-    return this.upvoteIds;
-  }
-
-  /**
-   * @returns {int} userId;
-   */
-  function getUser(){
-    return this.userId;
-  }
-
-  /**
-   * @returns {int} voteSum;
-   */
-  function getVoteSum(){
-    var sum = 0;
-    for (upvote in this.upvoteIds) {
-      sum += 1;
-    }
-    for (downvote in this.downvoteIds){
-      sum -= 1;
-    }
-    return sum;
-  }
-
-  /**
-   * @returns {int} UpvoteSum;
-   */
-  function getUpvoteSum(){
-    return this.upvoteIds.length;
-  }
-
-  /**
-   * @returns {int} DownvoteSum;
-   */
-  function getDownvoteSum(){
-    return this.downvoteIds.length;
-  }
-
-
-  /**
-   * 
-   * @param {int} userId 
-   * @returns {boolean} (if the user downvoted)
-   */
-  function userDownvoted(userId){
-    if (searchVoteArray(this.downvoteIds, this.userId)===-1){
-      return false;
-    }
-
-    else {
-      return true;
-    }
-  }
-
-  /**
-   * 
-   * @param {int} userId 
-   * @returns {boolean} (if the user upvoted)
-   */
-  function userUpvoted(userId){
-    if (searchVoteArray(this.upvoteIds, this.userId)===-1){
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-
-  /**
-   * 
-   * @param {boolean} up
-   */
-  function addVote(up){
-    if (up){
-      this.upvoteIds.push(this.userId);
-      upvoteIds.sort(function(a,b){return a-b});
-    }
-    else {
-      this.downvoteIds.push(this.userId);
-      downvoteIds.sort(function(a,b){return a-b});
-    }
-    return;
-  }
-
-  /**
-   * 
-   * @param {boolean} up
-   */
-  function removeVote(up, index){
-    if (up){
-      if (index != -1){
-        this.upvoteIds.splice(index, 1);
-      }
-      
-    }
-    else {
-      if (index != -1){
-        this.downvoteIds.splice(index, 1);
-      }     
-
-    }
-    return;
-
-  }
-
-  function vote(userId, up){
-    if (up){
-      var upIndex = searchVoteArray(this.upvoteIds, this.userId);
-      if (upIndex != -1){
-        removeVote(up, upIndex);
-      }
-      else {
-        var downIndex = searchVoteArray(this.downvoteIds, this.userId);
-        if (downIndex != -1){
-          removeVote(!up, downIndex);
+        // PRIVATE method definitions.
+        /**
+        * @returns {int} Id;
+        */
+        function getId() {
+            return this.Id;
         }
-        addVote(up);
-      }
-    }
-    else {
-      var downIndex = searchVoteArray(this.downvoteIds, this.userId);
-      if (downIndex != -1){
-        removeVote(up, downIndex);
-      }
-      else {
-        var upIndex = searchVoteArray(this.upvoteIds, this.userId);
-        if (upIndex != -1){
-          removeVote(!up, upIndex);
+
+        /**
+        * @returns {int} plantId;
+        */
+        function getPlantId() {
+            return this.plantId;
         }
-        addVote(up);
-      }
-  }
 
-  
-  function searchVoteArray(voteArray, userId){
-    var first = 0;
-    var mid;
-    var last = voteArray.length;
-    var found = false;
-    var result = -1;
+        /**
+        * @returns {int} userId;
+        */
+        function getUserId() {
+            return this.userId;
+        }
 
-    while (first <= last && !found){
-      mid = (first+last)/2;
-      if (userId === voteArray[mid]){
-        found = true;
-        result = mid;
-      }
-      else if (userId < voteArray[mid]){
-        last = mid - 1;
-      }
-      else {
-        first = mid + 1;
-      }
+        /**
+        * @returns {Date} uploadDate;
+        */
+        function getUploadDate() {
+            return this.uploadDate;
+        }
+
+        /**
+        * @returns {int[]} upvoteIds;
+        */
+        function getUpvoteIds() {
+            return this.upvoteIds;
+        }
+
+        /**
+         * @returns {int[]} downvoteIds;
+         */
+        function getDownvoteIds() {
+            return this.downvoteIds;
+        }
+
+        /**
+         * 
+         * @param {int} userId 
+         * @returns {boolean} (if the user upvoted)
+         */
+        function userUpvoted(userId) {
+            if (searchVoteArray(this.upvoteIds, this.userId) === -1) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        /**
+         * 
+         * @param {int} userId 
+         * @returns {boolean} (if the user downvoted)
+         */
+        function userDownvoted(userId) {
+            if (searchVoteArray(this.downvoteIds, this.userId) === -1) {
+                return false;
+            }
+
+            else {
+                return true;
+            }
+        }
+
+        /**
+         * @returns {int} voteSum;
+         */
+        function getVoteSum() {
+            var sum = 0;
+            for (upvote in this.upvoteIds) {
+                sum += 1;
+            }
+            for (downvote in this.downvoteIds) {
+                sum -= 1;
+            }
+            return sum;
+        }
+
+        function vote(userId, up) {
+            if (up) {
+                var upIndex = searchVoteArray(this.upvoteIds, this.userId);
+                if (upIndex != -1) {
+                    removeVote(up, upIndex);
+                }
+                else {
+                    var downIndex = searchVoteArray(this.downvoteIds, this.userId);
+                    if (downIndex != -1) {
+                        removeVote(!up, downIndex);
+                    }
+                    addVote(up);
+                }
+            }
+            else {
+                var downIndex = searchVoteArray(this.downvoteIds, this.userId);
+                if (downIndex != -1) {
+                    removeVote(up, downIndex);
+                }
+                else {
+                    var upIndex = searchVoteArray(this.upvoteIds, this.userId);
+                    if (upIndex != -1) {
+                        removeVote(!up, upIndex);
+                    }
+                    addVote(up);
+                }
+            }
+        }
+
+        // PRIVATE method definitions.
+        function searchVoteArray(voteArray, userId) {
+            var first = 0;
+            var mid;
+            var last = voteArray.length;
+            var found = false;
+            var result = -1;
+
+            while (first <= last && !found) {
+                mid = (first + last) / 2;
+                if (userId === voteArray[mid]) {
+                    found = true;
+                    result = mid;
+                }
+                else if (userId < voteArray[mid]) {
+                    last = mid - 1;
+                }
+                else {
+                    first = mid + 1;
+                }
+            }
+            return result;
+        }
+
+        /**
+         * 
+         * @param {boolean} up
+         */
+        function addVote(userId, up) {
+            if (up) {
+                this.upvoteIds.push(userId);
+                upvoteIds.sort(function (a, b) { return a - b });
+            }
+            else {
+                this.downvoteIds.push(userId);
+                downvoteIds.sort(function (a, b) { return a - b });
+            }
+            return;
+        }
+
+        /**
+         * 
+         * @param {boolean} up
+         */
+        function removeVote(up, index) {
+            if (up) {
+                if (index != -1) {
+                    this.upvoteIds.splice(index, 1);
+                }
+            }
+            else {
+                if (index != -1) {
+                    this.downvoteIds.splice(index, 1);
+                }
+            }
+            return;
+
+        }
     }
-    return result;
-  }
 }
