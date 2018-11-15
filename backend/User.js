81ce739436368b986749e38eb74727729e9239c3
Nathaniel Carr <carr0170@mylaurier.ca>
2018-11-14 22:06:53 -0500

    Documentation changes to User.js.

--- backend/User.js
+++ backend/User.js
@@ -1,5 +1,5 @@
 "use strict";
-import Ban from './Ban.js'
+import Ban from './Ban.js';
 
 const BAN_BASE = 3;
 
@@ -9,8 +9,11 @@ const BAN_BASE = 3;
  */
 export default class User {
     /**
-     * @param {Ban[]} bans The list of Bans associated with the User
-     * @param {int} userId The ID of the User
+     * @desc Constructor for the User class.
+     * @author Saje Bailey
+     * @param {Ban[]} bans The list of Bans associated with the User.
+     * @param {Number} userId The ID of the User. Integer.
+     * @constructor
      */
     constructor(userId, bans) {
         // PRIVATE attributes.
@@ -24,21 +27,23 @@ export default class User {
         this.isBanned = isBanned;
 
         // PUBLIC method definitions.
-        // getters
         /**
+         * @author Saje Bailey
          * @returns {Ban[]} The list of Bans associated with the User.
          */
         function getBans() {
             return _bans;
         }
         /**
-         * @returns {int} The ID of the User.
+         * @author Saje Bailey
+         * @returns {Number} The ID of the User. Integer.
          */
         function getId() {
             return _id;
         }
         /**
-         * @param {int} adminId The ID of the Admin banning the User.
+         * @author Saje Bailey
+         * @param {Number} adminId The ID of the Admin banning the User. Integer.
          */
         function ban(adminId) {
             let expiration = new Date();
@@ -49,7 +54,9 @@ export default class User {
             _bans[_bans.length] = new Ban(adminId, expiration);
         }
         /**
-         * @returns {boolean} true iff the User is currently banned.
+         * @author Saje Bailey
+         * @author Nathaniel Carr
+         * @returns {Boolean} true iff the User is currently banned.
          */
         function isBanned() {
             return (_bans.length > 0) && (new Date().getTime() < new Date(_bans[_bans.length - 1]).getTime());
