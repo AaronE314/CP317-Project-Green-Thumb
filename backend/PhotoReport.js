712ad65bffa047308b97f4625a9b4cbb2f3ac40b
Nathaniel Carr <carr0170@mylaurier.ca>
2018-11-14 22:10:04 -0500

    Documentation, naming standardization to PhotoReport.js.

--- backend/PhotoReport.js
+++ backend/PhotoReport.js
@@ -1,110 +1,70 @@
+"use strict";
 /** 
+ * @desc The PhotoReport class.
  * @author Scott Peebles
  * */
+export default class PhotoReport {
+	/**
+	 * @desc The PhotoReport class constructor.
+	 * @author Scott Peebles
+	 * @param {Number} id The PhotoReport ID. Integer.
+	 * @param {Number} photoId The ID of the reported Photo. Integer.
+	 * @param {Number} userId The ID of the User who made the PhotoReport. Integer.
+	 * @param {String} reportText The text of the PhotoReport.
+	 * @param {*=} reportDate The Date when the PhotoReport was created.
+	 * @constructor
+	 */
+	constructor(id, photoId, userId, reportText, reportDate) {
+		// PRIVATE attributes.
+		let _id = id;
+		let _photoId = photoId;
+		let _userId = userId;
+		let _reportText = reportText;
+		let _reportDate = reportDate !== undefined ? new Date(reportDate) : new Date();
 
-class PhotoReport{
-	
-	let adminId = -1;
-	let handleDate = new Date();
-	let id = -1;
-	let photoId = -1;
-	let reportDate = new Date();
-	let reportText = "";
-	let userId = -1;
-	let handled = false;
-	
-	/**
-	 * @param {int} photoId
-     * @param {int} userId
-	 * @param {int} photoReportId
-     * @constructor
-     */
+		// PUBLIC methods.
+		this.getId = getId;
+		this.getPhotoId = getPhotoId;
+		this.getUserId = getUserId;
+		this.getReportText = getReportText;
+		this.getReportDate = getReportDate;
 
-    constructor(photoId, userId, photoReportId) {
-        this.photoId = photoId;
-        this.userId = userId;
-		this.photoReportId = photoReportId;
-
-      }
-	  
-	  
-	/**
-     * @returns {AdminAction} AdminAction;
-     */
-	function getAdminAction(){
-		
-		return AdminAction;
-	}
-	
-	/**
-     * @returns {int} adminId;
-     */
-	function getAdminId(){
-		
-		return adminId;
-	}
-	
-	/**
-     * @returns {Date} handleDate;
-     */
-	function getHandleDate(){
-		
-		return handleDate;
-	}
-	
-	/**
-     * @returns {int} photoId;
-     */
-	function getPhotoId(){
-		return photoId;
-	}
-	
-	/**
-     * @returns {Date} reportDate;
-     */
-	function getReportDate(){
-		return reportDate;
-	}
-	
-	/**
-     * @returns {String} reportText;
-     */
-	function getReportText(){
-		return reportText;
-	}	
-		
-	/**
-     * @returns {int} userId;
-     */
-	function getUserId(){
-		return userId;
-	}
-	
-	
-	/**
-     * @returns {boolean}(If report is handled)
-     */
-	function is_Handled(){
-		
-		if(handled == true){ 
-			return true;
+		// PUBLIC method defintions.
+		/**
+		 * @author Scott Peebles
+		* @returns {Number} The PhotoReport ID. Integer.
+		*/
+		function getId() {
+			return _id;
 		}
-		else{
-			return false;
+		/**
+		 * @author Scott Peebles
+		* @returns {Number} The ID of the reported Photo. Integer.
+		*/
+		function getPhotoId() {
+			return _photoId;
 		}
-		
-		
-	}
-	
-	public handle(adminID, AdminAction){
-		
-		AdminAction();
-		handled = true;
-		
-		
-		
-		
-	}
+		/**
+		 * @author Scott Peebles
+		* @returns {Number} The ID of the reported Photo. Integer.
+		*/
+		function getUserId() {
+			return _userId;
+		}
+		/**
+		 * @author Scott Peebles
+		* @returns {String} The text of the PhotoReport.
+		*/
+		function getReportText() {
+			return _reportText;
+		}
+		/**
+		 * @author Scott Peebles
+		* @returns {*} The Date when the PhotoReport was created.
+		*/
+		function getReportDate() {
+			return _reportDate;
+		}
 
-
+	}
 }
