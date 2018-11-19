65ad8259d9599e9d1c90306dd31bfe4d3a90c153
HallowedHalcyon <43186991+HallowedHalcyon@users.noreply.github.com>
2018-11-19 02:31:56 -0500

    This is an actual class now
    With what' going on in the backend chat, changes may have to be made pretty soon so I thought I'd commit now.

--- backend/Admin.js
+++ backend/Admin.js
@@ -1,4 +1,53 @@
+"use strict";
 
-// inherits or wraps around user
-// bans is a lis of bans the admin has given not bans on the admin
-// privae methods user has admin doesn't
+import Ban from './Ban.js'
+
+const User = require("./User.js");
+
+/**
+ * @desc The Admin class
+ * @author Saje Bailey
+ */
+class Admin extends User {
+    /**
+     * @desc Constructor for the Admin class.
+     * @author Saje Bailey
+     * @param {Ban[]} bans The list of Bans issued by Admin.
+     * @param {Number} userId The ID of the Admin. Integer.
+     * @constructor
+     */
+    constructor(userId, bans) {
+        super(userId, bans);
+
+        this.addBan = addBan;
+        this.ban = ban;
+        this.isBanned = isBanned;
+
+        /**
+         * @author Saje Bailey
+         * @returns {Ban[]} The list of Bans issued by Admin.
+         */
+        function addBan(userId, expiration) {
+            this._bans[this._bans.length] = new Ban(userId, expiration);
+        }
+
+        /**
+         * @author Saje Bailey
+         * @param {Number} adminId The ID of the Admin banning the User. Integer.
+         * @throws exception, an Admin cannot be banned
+         */
+        function ban(adminId) {
+            throw "An Admin cannot be banned.";
+        }
+
+        /**
+         * @author Saje Bailey
+         * @returns {Boolean} false always.
+         */
+        function isBanned() {
+            return false;
+        }
+    }
+}
+
+module.exports = Admin;
