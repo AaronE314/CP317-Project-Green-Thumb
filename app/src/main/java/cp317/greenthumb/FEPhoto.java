d64331b160331425f318b7e26d7338e5e64a2f48
epic-matt <buch6620@gmail.com>
2018-11-21 14:04:57 -0500

    added java classes to represent the json classes in the backend-testing branch for use by the frontend team.

--- app/src/main/java/cp317/greenthumb/FEPhoto.java
+++ app/src/main/java/cp317/greenthumb/FEPhoto.java
@@ -1,4 +1,41 @@
 package cp317.greenthumb;
 
+import java.util.Date;
+
 public class FEPhoto {
+    private int id;
+    private int plantid;
+    private int userid;
+    private char[] image;
+    private Date uploadData;
+    private int[] upvoteids;
+    private int[] downvoteids;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPlantid() {
+        return plantid;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public char[] getImage() {
+        return image;
+    }
+
+    public Date getUploadData() {
+        return uploadData;
+    }
+
+    public int[] getUpvoteids() {
+        return upvoteids;
+    }
+
+    public int[] getDownvoteids() {
+        return downvoteids;
+    }
 }
--- app/src/main/java/cp317/greenthumb/FEPhotoReport.java
+++ app/src/main/java/cp317/greenthumb/FEPhotoReport.java
@@ -1,4 +1,31 @@
 package cp317.greenthumb;
 
+import java.util.Date;
+
 public class FEPhotoReport {
+    private int _id;
+    private int _photoid;
+    private int _userid;
+    private String reportText;
+    private Date date;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public int get_photoid() {
+        return _photoid;
+    }
+
+    public int get_userid() {
+        return _userid;
+    }
+
+    public String getReportText() {
+        return reportText;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 }
