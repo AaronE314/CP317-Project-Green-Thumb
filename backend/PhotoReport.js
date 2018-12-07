d26b26fe18486eb886575b3019dab08ab46d47da
Nathaniel Carr <carr0170@mylaurier.ca>
2018-12-07 17:15:34 -0500

    Made constructor id arg optional.

--- backend/PhotoReport.js
+++ backend/PhotoReport.js
@@ -7,19 +7,19 @@ class PhotoReport {
 	/**
 	 * @desc The PhotoReport class constructor.
 	 * @author Scott Peebles
-	 * @param {Number} id The PhotoReport ID. Integer.
+	 * @param {Number=} id The PhotoReport ID. Integer.
 	 * @param {Number} photoId The ID of the reported Photo. Integer.
 	 * @param {Number} userId The ID of the User who made the PhotoReport. Integer.
 	 * @param {String} reportText The text of the PhotoReport.
 	 * @param {*=} reportDate The Date when the PhotoReport was created.
 	 * @constructor
 	 */
-	constructor(id, photoId, userId, reportText, reportDate) {
+	constructor(photoId, userId, reportText, id, reportDate) {
 		// PRIVATE attributes.
-		let _id = id;
 		let _photoId = photoId;
 		let _userId = userId;
 		let _reportText = reportText;
+		let _id = id; // Fine if initially undefined.
 		let _reportDate = reportDate !== undefined ? new Date(reportDate) : new Date();
 
 		// PUBLIC methods.
