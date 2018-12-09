0a19543daed0f70cf04c2379fd55d2d9602d7838
AdamCassidy <43080428+AdamCassidy@users.noreply.github.com>
2018-12-09 01:18:10 -0500

    Added it functions for testing.

--- test/backend/Admin.js
+++ test/backend/Admin.js
@@ -4,4 +4,20 @@ const Admin = require("../../backend/Admin");
 
 describe("Admin Tests", () => {
 
+    it("nextBanExpirationDate() should return a date",
+        /**
+         * @author Adam Cassidy
+         */
+        () => {
+            let date = new Date();
+            assert.equal(Admin.nextBanExpirationDate(), date);
+        });
+
+    it("isBanned() should return false",
+        /**
+         * @author Adam Cassidy
+         */
+        () => {
+            assert.equal(Admin.isBanned(), false);
+        });
 });
