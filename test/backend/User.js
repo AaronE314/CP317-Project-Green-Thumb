5575d7fdff1f346761ee446c82bedc8e8df964b8
Nathaniel Carr <carr0170@mylaurier.ca>
2018-12-10 15:11:40 -0500

    Added Ban import; fixed some logic errors (JSON.stringify must be used to compare arrays).

--- test/backend/User.js
+++ test/backend/User.js
@@ -1,61 +1,51 @@
 "use strict";
 const assert = require("assert");
 const User = require("../../backend/User");
+const Ban = require("../../backend/Ban");
 
 describe("User Tests", () => {
-      it("getBans() should return the private list of bans attribute.",
+    it("getBans() should return the private list of bans attribute.",
         /**
          * @author Adam Cassidy
          */
         () => {
-            let ban = new Ban(1, 2, new Date(0),9);
-            let banList = new Ban[ban];
-            let user = new User(32,banList);
-            assert.equals(user.getBans(), banList);
-      });
-      it("getId() should return the private id attribute.",
+            let user = new User(32, [new Ban(1, 2, new Date(0), 9)]);
+            assert.equal(JSON.stringify(user.getBans()), JSON.stringify([new Ban(1, 2, new Date(0), 9)]));
+        });
+    it("getId() should return the private id attribute.",
         /**
          * @author Adam Cassidy
          */
         () => {
-            let user = new User(32,banList);
-            assert.equals(user.getId(), 32);
-      });
-      it("isBanned() should return a boolean that's true if the User's banned.",
+            let user = new User(32, [new Ban(1, 2, new Date(0), 9)]);
+            assert.equal(user.getId(), 32);
+        });
+    it("isBanned() should return a boolean that's true if the User's banned.",
         /**
          * @author Adam Cassidy
          */
         () => {
-            let ban = new Ban(1, 2, new Date(0),9);
-            let banList = new Ban[ban];
-            let user = new User(32,banList);
-            assert.equals(user.isBanned(), true);
-            let secondUser = new User(43,Ban[]);
-            assert.equals(user.isBanned(), false);
-      });
-      it("nextBanExpirationDate() should return a date.",
+            let user = new User(32, [new Ban(1, 2, new Date(new Date().getTime() + 10000), 9)]);
+            assert(user.isBanned());
+        });
+    it("nextBanExpirationDate() should return a date.",
         /**
          * @author Adam Cassidy
          */
         () => {
-            let ban = new Ban(1, 2, new Date(0),9);
-            let banList = new Ban[ban];
-            let user = new User(32,banList);
-            assert.equals(user.nextBanExpirationDate(), Date);
-      });
-      it("toJSON() should produce an object containing a copy of the attributes of the User object",
+            let user = new User(32, [new Ban(1, 2, new Date(0), 9)]);
+            assert(user.nextBanExpirationDate() instanceof Date);
+        });
+    it("toJSON() should produce an object containing a copy of the attributes of the User object",
         /**
          * @author Adam Cassidy
          */
         () => {
-            let ban = new Ban(1, 2, new Date(0),9);
-            let banList = new Ban[ban];
-            let user = new User(32,banList);
+            let user = new User(32, [new Ban(1, 2, new Date(0), 9)]);
             let toJSON = user.toJSON();
             assert.notDeepStrictEqual(user, toJSON);
             assert.equal(toJSON.id, 32);
-            assert.equal(toJSON.bans, banList);
             assert(!(toJSON.reportDate < new Date(0)) && !(toJSON.reportDate > new Date(0)));
-});
-  
+        });
+
 });
