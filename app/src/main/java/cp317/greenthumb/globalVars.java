37513a73d336956c26e10beb5d31a81a69dfd0f0
Jackson Newlands <32347226+jackson097@users.noreply.github.com>
2018-12-10 14:03:31 -0500

    Update globalVars.java
    Changed font_scale to font_size which is initialized and then changed in settings

--- app/src/main/java/cp317/greenthumb/globalVars.java
+++ app/src/main/java/cp317/greenthumb/globalVars.java
@@ -1,5 +1,5 @@
 package cp317.greenthumb;
 
 public interface globalVars {
-    double text_scale =1.00;
+    double text_size = 52.5;    // 52.5 seems to be the default font... the value that initializes the fontslider to be centered
 }
