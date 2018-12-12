97ecb8859331d05da092663d90d1632be05d4326
Tim <123timmcgill@gmail.com>
2018-12-12 14:30:12 -0500

    Forgot to commit ScanActivity.

---
+++ app/src/main/java/cp317/greenthumb/ScanActivity.java
@@ -0,0 +1,14 @@
+package cp317.greenthumb;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ScanActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+
+}
