09b216789088f70d3a272151e0d82746227a898b
Kerpal <luci3330@mylaurier.ca>
2018-12-12 15:26:47 -0500

    Fixed colors

--- app/src/main/java/cp317/greenthumb/ScanActivity.java
+++ app/src/main/java/cp317/greenthumb/ScanActivity.java
@@ -2,13 +2,17 @@ package cp317.greenthumb;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 
 public class ScanActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
+        setContentView(R.layout.activity_splash_screen);
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
