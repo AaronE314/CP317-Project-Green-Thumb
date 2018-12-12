0da647438ae272d037c8a8ae7007b7480071a770
Nathaniel Carr <carr0170@mylaurier.ca>
2018-12-12 15:46:21 -0500

    Updated unit tests to treat ids as String.

--- test/backend/Admin.js
+++ test/backend/Admin.js
@@ -9,7 +9,7 @@ describe("Admin Tests", () => {
          * @author Adam Cassidy
          */
         () => {
-            assert((new Admin(1)).nextBanExpirationDate() instanceof Date);
+            assert((new Admin("1")).nextBanExpirationDate() instanceof Date);
         });
 
     it("isBanned() should return false.",
@@ -17,6 +17,18 @@ describe("Admin Tests", () => {
          * @author Adam Cassidy
          */
         () => {
-            assert.equal((new Admin(1)).isBanned(), false);
+            assert.equal((new Admin("1")).isBanned(), false);
+        });
+
+    it("toJSON() should produce an object containing a copy of the attributes of the Admin object",
+        /**
+         * @author Adam Cassidy
+         */
+        () => {
+            let user = new Admin("32", []);
+            let toJSON = user.toJSON();
+            assert.notDeepStrictEqual(user, toJSON);
+            assert.equal(toJSON.id, "32");
+            assert(!(toJSON.reportDate < new Date(0)) && !(toJSON.reportDate > new Date(0)));
         });
 });
