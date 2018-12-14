fb9430886ac0cd1e0c9edecd52150efdd1325b70
Nathaniel Carr <carr0170@mylaurier.ca>
2018-12-14 12:11:59 -0500

    Changed !== to !=

--- backend/db/DBInterface.js
+++ backend/db/DBInterface.js
@@ -1033,8 +1033,8 @@ async function getNewestPlantPhotos(plantId, startIndex, max) {
                 'WHERE ph.plant_id = @plantId ORDER BY po.upload_date DESC'
             return await req.query(sqlQuery).then(function (recordset) {
                 ind = 0
-                if (recordset.recordset[0] !== null) {
-                    while (recordset[ind] !== null) {
+                if (recordset.recordset[0] != null) {
+                    while (recordset[ind] != null) {
                         photos.push(new Photo(recordset.recordset[ind].plant_id, recordset.recordset[ind].user_id, recordset.recordset[ind].image.toString('base64'), recordset.recordset[ind].photo_id, recordset.recordset[ind].upload_date, create_votes(recordset.recordset[ind].photo_id,1),create_votes(recordset.recordset[ind].photo_id,0)));
                         ind = ind + 1;
                     }
