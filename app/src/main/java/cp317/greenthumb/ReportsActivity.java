bcde00c6140518562782bd486c913c333f36b97a
daviddjukic <43214920+daviddjukic@users.noreply.github.com>
2018-12-15 21:56:06 -0500

    Update ReportsActivity.java
    Updated report activity after losing it

--- app/src/main/java/cp317/greenthumb/ReportsActivity.java
+++ app/src/main/java/cp317/greenthumb/ReportsActivity.java
@@ -7,10 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ReportsActivity extends AppCompatActivity {
-
+public class ReportsActivity extends AppCompatActivity implements AsyncResponse {
+    private ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -24,6 +25,48 @@ public class ReportsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        Button mStartReport = (Button) findViewById(R.id.startReportButton);
+        mStartReport.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(ReportsActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.activity_reports,null);
+                final EditText mPhotoId = (EditText) mView.findViewById(R.id.photoId);
+                final EditText mDate = (EditText) mView.findViewById(R.id.reportDate);
+                final EditText mDetails = (EditText) mView.findViewById(R.id.reportDetails);
+                Button mSendReport = (Button) mView.findViewById(R.id.sendReportButton);
+
+                mSendReport.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view)   {
+                        if (!mPhotoId.getText().toString().isEmpty() && !mDate.getText().toString().isEmpty() && !mDetails.getText().toString().isEmpty()){
+
+                            Toast.makeText(ReportsActivity.this,getString(R.string.success_report_msg),Toast.LENGTH_SHORT).show();
+                            FEPhotoReport reportInfo = FEPhotoReport(null,mPhotoId,FEUser.this._userid,mDetails,mDate);
+
+                        }
+                        else {
+                            Toast.makeText(ReportsActivity.this,getString(R.string.error_report_msg),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+
+            }
+        } );
+
+
     }
+    public void backToGalleryView(View view){
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
